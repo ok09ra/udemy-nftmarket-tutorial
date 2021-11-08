10b654176a842e3c5a5a62c70b2113b8595f3c63
ok09ra <okudasota@gmail.com>
2021-11-09 00:05:14 +0900

    add ERC721 contracts

---
+++ src/contracts/ERC721.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ERC721{
+    /*
+    building out the minting function:
+        a. nft to point to an address
+        b. keep track of the token ids
+        c. keep track of token owner addresses to token ids
+        d. keep track of how many tokens an owner address has
+        e. create an event that emits a transfer log - contract address, where it is being minted to the id.
+    */
+
+}
